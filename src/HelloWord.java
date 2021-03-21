531c53a3221c6126a8837fe3b4f1576d2c314ab7
Liuth53 <72739656+Liuth53@users.noreply.github.com>
2021-03-22 07:58:37 +0800

    Initial commit

---
+++ src/HelloWord.java
@@ -0,0 +1,11 @@
+public class HelloWord {
+    public static void main(String[] args) {
+
+        IdentifierDemo wangTao = new IdentifierDemo("WangTao");
+        System.out.println("Hello!");
+        wangTao.eat("meat");
+        wangTao.sleep("man");
+        wangTao.playGames("LOL");
+
+    }
+}
