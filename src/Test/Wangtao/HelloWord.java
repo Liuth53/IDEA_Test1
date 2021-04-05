31b65c48c9f870052cf0e8b6b253860ba92de4b1
Liuth53 <72739656+Liuth53@users.noreply.github.com>
2021-04-05 10:00:16 +0800

    Default Changelist

---
+++ src/Test/Wangtao/HelloWord.java
@@ -0,0 +1,14 @@
+package Test.Wangtao;
+
+public class HelloWord {
+    public static void main(String[] args) {
+
+        Wangtao wangTao = new Wangtao("WangTao",18,"man","...");
+        System.out.println("Hello!");
+        wangTao.eat("meat");
+        wangTao.sleep("man");
+        wangTao.shangHao("LOL");
+        wangTao.State();
+
+    }
+}
